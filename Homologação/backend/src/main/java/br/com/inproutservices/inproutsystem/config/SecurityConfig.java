8a51191775e36cea9efecf1814e9d761e9aa2ebc
Guilherme Ferrari <guilhermesouzaferrari@gmail.com>
2025-09-15 16:32:38 -0300

    versão prod

--- Homologação/backend/src/main/java/br/com/inproutservices/inproutsystem/config/SecurityConfig.java
+++ Homologação/backend/src/main/java/br/com/inproutservices/inproutsystem/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://3.128.248.3"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
